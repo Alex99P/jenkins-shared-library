c921a22abaef213df409fafa167c13f481de0e69
alexandru.patroi <alexandru.patroi@opswat.com>
2023-03-22 16:31:13 +0200

    add jenkins-shared-lib branch

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 
 
-def call() {
+def call(String imageName) {
     echo 'Building the docker image'
     withCredentials([usernamePassword(credentialsId:'docker-hub-repo', passwordVariable: 'PASS',usernameVariable:'USER' )]){
-        sh 'docker build -t alexpatroi/my-jenkins:jma-3.1 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push alexpatroi/my-jenkins:jma-3.1'
+        sh "docker push $imageName"
     }
 }
