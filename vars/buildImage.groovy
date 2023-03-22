0c82bbd9445e71ac16223307bc0ec83557c86eb5
alexandru.patroi <alexandru.patroi@opswat.com>
2023-03-22 15:16:40 +0200

    add develop branch

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,7 +1,11 @@
 #!/usr/bin/env groovy
 
-import com.example.Docker
 
-def call(String imageName) {
-    return new Docker(this).buildDockerImage(imageName)
+def call() {
+    echo 'Building the docker image'
+    withCredentials([usernamePassword(credentialsId:'docker-hub-repo', passwordVariable: 'PASS',usernameVariable:'USER' )]){
+        sh 'docker build -t alexpatroi/my-jenkins:jma-3.1 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push alexpatroi/my-jenkins:jma-3.1'
+    }
 }
