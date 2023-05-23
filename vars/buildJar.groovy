061db947a8e59755e4a3b68584e4d2f4bf863350
alexandru.patroi <alexandru.patroi@opswat.com>
2023-05-23 13:26:36 +0300

    change the credential name

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/env groovy
 
+// we call in Jenkinsfile the name of this file "buildImage"
 def call() {
     echo "building the application for branch $BRANCH_NAME"
     sh 'mvn package'
