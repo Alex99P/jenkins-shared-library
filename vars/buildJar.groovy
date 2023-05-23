1ab57e48806519267f341468686ae8983dd2452e
alexandru.patroi <alexandru.patroi@opswat.com>
2023-05-23 14:44:42 +0300

    test

--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -3,5 +3,5 @@
 // we call in Jenkinsfile the name of this file "buildImage"
 def call() {
     echo "building the application for branch $BRANCH_NAME"
-    sh 'mvn package'
+    // sh 'mvn package'
 }
