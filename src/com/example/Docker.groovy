5a9d7ab2bd6e34d580bc3f21fccc0a6d9c9b9192
alexandru.patroi <alexandru.patroi@opswat.com>
2023-05-24 11:13:32 +0300

    test

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -9,16 +9,13 @@ class Docker implements Serializable {
         this.script = script
     }
 
-    // def buildDockerImage(String imageName) {
-    //     script.echo "building the docker image..."
-    //     script.sh "docker build -t $imageName ."
-    // }
-      def buildDockerImage() {
+    def buildDockerImage(String imageName) {
         script.echo "building the docker image..."
-        script.sh "docker build -t alexpatroi/my-jenkins:jma-1.0 ."
+        script.sh "docker build -t $imageName ."
     }
 
     def dockerLogin() {
+        script.echo "login"
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
         }
