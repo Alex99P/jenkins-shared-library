12f20dfb80f30028e8012fbbd9b2621a4eb1ba7a
alexandru.patroi <alexandru.patroi@opswat.com>
2023-05-24 11:56:23 +0300

    add login and push func

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -16,7 +16,7 @@ class Docker implements Serializable {
         // script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         //     script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
         // script.sh "docker push $imageName"
-    }
+    // }
     }
     def dockerLogin() {
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
